72ddb6b25feaac67f06317c05eb002957ab92340
rpopovnin <kho.dhj@gmail.com>
2020-05-11 15:15:43 +0400

    repair circuit breaker

--- product/src/main/java/com/griddynamics/product/controller/ProductController.java
+++ product/src/main/java/com/griddynamics/product/controller/ProductController.java
@@ -1,40 +1,31 @@
 package com.griddynamics.product.controller;
 
-import com.griddynamics.product.exception.ServiceUnavailableException;
-import com.griddynamics.product.model.ProductEntity;
+import com.griddynamics.product.exception.InventoryDataNotFoundException;
 import com.griddynamics.product.service.ProductService;
-import com.netflix.hystrix.exception.HystrixRuntimeException;
-import com.netflix.hystrix.exception.HystrixTimeoutException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/products")
+@Slf4j
 public class ProductController {
 
     private final ProductService productService;
 
     @GetMapping("/sku/{sku}")
-    private List<ProductEntity> getAvailableProductsBySku(@PathVariable String sku) {
+    private ResponseEntity getAvailableProductsBySku(@PathVariable String sku) {
         try {
             return productService.getAvailableProducts(sku);
-        } catch (HystrixRuntimeException e) {
-            throw poorPeopleExceptionHandler(e);
+        } catch (InventoryDataNotFoundException | HttpClientErrorException.NotFound e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
-    }
-
-    private RuntimeException poorPeopleExceptionHandler(HystrixRuntimeException e) {
-        if (e.getFallbackException().getCause() instanceof HystrixTimeoutException)
-            return new ServiceUnavailableException("timed out");
-        else if (e.getFallbackException().getCause() instanceof RuntimeException)
-            return new ServiceUnavailableException("threshold has been reached");
-        else
-            return new RuntimeException(e.getFallbackException().getCause());
     }
 }
--- product/src/main/java/com/griddynamics/product/service/ProductService.java
+++ product/src/main/java/com/griddynamics/product/service/ProductService.java
@@ -7,6 +7,7 @@ import com.griddynamics.product.model.ProductEntity;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixException;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.hystrix.exception.HystrixTimeoutException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
@@ -37,15 +38,16 @@ public class ProductService {
     private final RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "fallbackMethodTest",
+            commandKey = "getAvailableProducts",
             commandProperties = {
                     @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
                     @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),
                     @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "30000"),
-                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "3")
-            },
-            ignoreExceptions = {InventoryDataNotFoundException.class},
-            raiseHystrixExceptions = {HystrixException.RUNTIME_EXCEPTION})
-    public List<ProductEntity> getAvailableProducts(String sku) {
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "3"),
+            },ignoreExceptions = {InventoryDataNotFoundException.class, HttpClientErrorException.NotFound.class})
+    //ignoreExceptions = {InventoryDataNotFoundException.class}
+    //raiseHystrixExceptions = {HystrixException.RUNTIME_EXCEPTION}
+    public ResponseEntity getAvailableProducts(String sku) throws ServiceUnavailableException {
         ResponseEntity<ProductEntity[]> productEntityResponseEntity =
                 restTemplate.getForEntity(CATALOG_API_PATH + "/sku/{sku}", ProductEntity[].class, sku);
 
@@ -58,25 +60,34 @@ public class ProductService {
                 .collect(toList()), headers);
 
         ResponseEntity<InventoryDTO[]> inventoryProductEntityResponseEntity;
-        try {
-            inventoryProductEntityResponseEntity = restTemplate.postForEntity(INVENTORY_API_PATH, request, InventoryDTO[].class);
-        } catch (HttpClientErrorException ex) {
-            if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
-                throw new InventoryDataNotFoundException("inventory product data not found");
-            }
-            throw new IllegalStateException(ex);
+        inventoryProductEntityResponseEntity = restTemplate.postForEntity(INVENTORY_API_PATH, request, InventoryDTO[].class);
+
+        if (inventoryProductEntityResponseEntity.getStatusCode() == HttpStatus.NOT_FOUND) {
+            log.error("inventory product data not found");
+            throw new InventoryDataNotFoundException("inventory product data not found");
         }
 
         Map<String, Long> inventoryDataMap = Arrays.stream(inventoryProductEntityResponseEntity.getBody())
                 .collect(toMap(InventoryDTO::getId, InventoryDTO::getAvailable));
-        return availableProducts.stream()
+        return ResponseEntity.ok(availableProducts.stream()
                 .filter(e -> inventoryDataMap.containsKey(e.getId()) && inventoryDataMap.get(e.getId()) > 0)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 
-    private List<ProductEntity> fallbackMethodTest(String sku) {
-        log.info("fallback method was invoked, sku = {}", sku);
-
-        throw new ServiceUnavailableException("service unavailable");
+    private ResponseEntity fallbackMethodTest(String sku, Throwable e) {
+        log.error("fallback method was invoked, sku = {}", sku);
+        if (e instanceof HystrixTimeoutException) {
+            log.error("timed out", e);
+        } else if (e instanceof InventoryDataNotFoundException || e instanceof HttpClientErrorException) {
+            //this branch won't be called since we ignore such exceptions. handling in controller
+            //therefore wont affect threshold
+            log.error("inventory data isn't found", e);
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        } else if (e instanceof RuntimeException) {
+            log.error("threshold has been reached", e);
+        } else {
+            log.error("something else", e);
+        }
+        return new ResponseEntity(HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
